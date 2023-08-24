7086aab5422a8951de1fb25e2df831a722df524f
heather-codecov <109541506+heather-codecov@users.noreply.github.com>
2023-08-24 11:10:49 -0700

    Assets to public (#18)
    * moved assests out of src and moved modal further right
    
    * moved assets to public folder
    
    * updated path for close widget
    
    * Update island1.jsx
    
    removed "public" in close-widget path

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -5,10 +5,12 @@ import { useControls } from "leva";
 import { Player } from "./components/Player";
 import { Ground } from "./components/Ground";
 import { Island1 } from "./components/Island1";
+
 // import { Island2 } from "./components/Island2"; // Model doesnt work
 import { Island3 } from "./components/Island3";
 import { Island4 } from "./components/Island4";
 import { Island5 } from "./components/Island5";
+
 
 import { FishingSpots } from "./components/FishingSpots";
 
@@ -23,9 +25,11 @@ function Scene() {
       <Ground />
       <FishingSpots />
       <Island1 />
+
       <Island3 />
       <Island4 />
       <Island5 />
+
     </Physics>
   );
 }
